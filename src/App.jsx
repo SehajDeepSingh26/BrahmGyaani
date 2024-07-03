1feddf3918ea50c6944ceeecdf666e6b7950a661
Sehajdeep Singh <114660532+SehajDeepSingh26@users.noreply.github.com>
2024-07-03 12:05:39 +0530

    initialization of folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 
-import './App.css'
 
 function App() {
 
   return (
     <>
-        <h1>BrahmGyaani Solutions</h1>
+        <h1 className="bg-slate-400">BrahmGyaani Solutions</h1>
     </>
   )
 }
